cf709db0f1ed5076d94e0971d4d93f61a8dfc990
Nicholas Lovdahl <nlovdahl@outlook.com>
2020-10-12 12:40:44 -0500

    Set the screen to a lime green.

--- src/reset_interrupt_handler.s
+++ src/reset_interrupt_handler.s
@@ -17,10 +17,49 @@
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 .include "interrupt_handlers.inc"
+.include "system_macros.inc"
+.include "system_aliases.inc"
 
 .segment "RESET_INTERRUPT_HANDLER"
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 .proc ResetHandler
+	sei ; disable interrupts
+	clc ; enter native mode (switch from 8-bit mode to 16-bit mode)
+	xce
+	
+	; initialize the CPU: set register sizes, set the stack pointer, etc.
+	SET_BIN_MODE ; use binary mode and make registers A, X, and Y 16-bits
+	SET_ACCUM_16_BIT
+	SET_INDEX_16_BIT
+	
+	lda #$1FFF ; set the stack pointer at this address
+	tcs
+	
+	lda #$0000 ; set the direct page to this address
+	tcd
+	
+	SET_ACCUM_8_BIT ; make register A 8-bits now
+	
+	jml DataBankRegisterReset ; update the data bank register by jumping here
+DataBankRegisterReset:
 
+	; initialize the system's hardware registers
+	lda #$80 ; disable drawing to screen and darken the screen
+	sta INIDISP
+	
+	lda #$70 ; low byte of color first
+	sta CGDATA
+	lda #$03 ; then the high byte
+	sta CGDATA
+	
+	; prepare to turn over controll of the system (restart some things)
+	lda #$0F ; turn on the screen with full brightness
+	sta INIDISP
+	lda #$81 ; allow V-Blanks again and start automatically polling controllers
+	sta NMITIMEN
+	
+	cli ; enable interrupts again
+	
+	wai ; stall the processor...
 .endproc
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
